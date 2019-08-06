4ff4a763d4ba896e8e0f2f52d5ea77384435f952
prithvi venu <prithviv30@gmail.com>
2019-08-06 18:33:06 +0530

    added controls for expand and collapse

--- ZTasks/MainPage.xaml.cs
+++ ZTasks/MainPage.xaml.cs
@@ -36,6 +36,18 @@ namespace ZTasks
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+            if (MySplitView.IsPaneOpen)
+            {
+                TitleTextBlock.Text = "ZTasks";
+                double left = 20, top = 10, right = 220, bottom = 0;
+                TitleTextBlock.Margin = new Thickness(left, top, right, bottom);
+            }
+            else
+            {
+                TitleTextBlock.Text = "";
+                double left = 0, top = 0, right = 10, bottom = 0;
+                TitleTextBlock.Margin = new Thickness(left, top, right, bottom);
+            }
         }
     }
 }
