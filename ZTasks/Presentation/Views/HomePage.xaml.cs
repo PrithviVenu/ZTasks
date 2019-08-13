b3a00213999691fc4477abb4759873868564e045
prithvi venu <prithviv30@gmail.com>
2019-08-13 21:20:13 +0530

    Created Data Template For the Task List

--- ZTasks/Presentation/Views/HomePage.xaml.cs
+++ ZTasks/Presentation/Views/HomePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ZTasks.Domain.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,6 +24,8 @@ namespace ZTasks.Presentation.Views
     /// </summary>
     public sealed partial class HomePage : Page
     {
+        private ObservableCollection<Task> tasks;
+
         public HomePage()
         {
             this.InitializeComponent();
