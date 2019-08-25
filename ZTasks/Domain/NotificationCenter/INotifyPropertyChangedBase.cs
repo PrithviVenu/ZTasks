8cae4c7f9352c05b8015106fdc5848a34d5dd453
prithvi venu <prithviv30@gmail.com>
2019-08-25 08:34:50 +0530

    Created NotificationCenter

---
+++ ZTasks/Domain/NotificationCenter/INotifyPropertyChangedBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTasks.Domain.NotificationCenter
+{
+    public class INotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
